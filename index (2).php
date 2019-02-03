aeac177748bd5e6205443e2c05a6c72a24ff01ff
zahidali85 <zahidali85@hitmail.com>
2019-02-03 15:18:48 +0500

    merge zahid with web

---
+++ index (2).php
@@ -0,0 +1,60 @@
+<?php include('header.php');
+?>
+	
+	<!-- <div class="container">
+
+
+		<div class="row">
+			<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 col-xl-4">
+				<h1>ui learning</h1>
+				<img src="assets/img/1.jpg" class="img-fluid rounded">
+				<p align="justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<button class="btn-defult">Read more..</button>
+			</div>
+		<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 col-xl-4">
+				<h1>ui learning</h1>
+				<img src="assets/img/2.jpg" class="img-fluid rounded">
+				<p align="justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<button class="btn-defult">learn more..</button>
+		</div>
+			<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 col-xl-4">
+				<h1>ui learning</h1>
+				<img src="assets/img/3.jpg" class="img-fluid rounded">
+				<p align="justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<button class="btn-defult">download</button>
+			
+			</div>
+		</div>
+	
+	</div> -->
+
+
+
+
+
+
+
+
+
+
+
+<?php require_once('footer.php');
+?>
+</body>
+
+</html> 
